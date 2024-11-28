ca933d1d2a325e382ac4af776b574eb5fe826875
Sameer Kadu <Samirkadu8@gmail.com>
2024-11-28 21:29:23 +0530

    Home page

---
+++ GoBus_Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Home from './components/Home.jsx';
+const App = () => {
+    return (
+        <main className="relateive min-h-screen w-screen overflow-x-hidden">
+           <Home />
+            <section className="z-0 min-h-screen bg-blue-500"/>
+        </main>
+    )
+}
+export default App;
