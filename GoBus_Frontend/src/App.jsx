340b93578e92cf9146f5f761629491eaad8600e4
Sameer Kadu <Samirkadu8@gmail.com>
2024-11-29 21:49:06 +0530

    about page started

--- GoBus_Frontend/src/App.jsx
+++ GoBus_Frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Home from './components/Home.jsx';
+import About from "./components/About.jsx";
 const App = () => {
     return (
         <main className="relateive min-h-screen w-screen overflow-x-hidden">
            <Home />
-            <section className="z-0 min-h-screen bg-blue-500"/>
+            <About />
         </main>
     )
 }
