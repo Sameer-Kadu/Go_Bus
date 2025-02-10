58979940ca46e3edcc1705c7ce7ddd1fbedbb366
Sameer Kadu <Samirkadu8@gmail.com>
2025-02-10 12:04:04 +0530

    Deploying, dockerfile and buildspec.yaml file added

--- GoBus_Backend/src/main/java/com/go_bus/GoBusSecurityApplication.java
+++ GoBus_Backend/src/main/java/com/go_bus/GoBusSecurityApplication.java
@@ -5,7 +5,7 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 //main class which help to start the program
 @CrossOrigin
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class GoBusSecurityApplication {
 
 	public static void main(String[] args) {
--- GoBus_Backend/src/main/java/com/go_bus/security/SecurityConfiguration.java
+++ GoBus_Backend/src/main/java/com/go_bus/security/SecurityConfiguration.java
@@ -49,10 +49,10 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // ✅ Allow Frontend Origin
+        configuration.setAllowedOrigins(List.of("*")); // ✅ Allow 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true); // ✅ Allow Cookies and Authentication Headers
+        configuration.setAllowCredentials(false); 
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
